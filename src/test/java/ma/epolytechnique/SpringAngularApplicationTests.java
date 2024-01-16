c9b09832b154135eb92419946c0cb6dc80b0a7c5
meryem5 <Meryem.bouanisse@e-polytechnique.ma>
2024-01-16 03:18:24 +0100

    first commit

---
+++ src/test/java/ma/epolytechnique/SpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.epolytechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
